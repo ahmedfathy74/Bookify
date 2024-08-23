c07611b5d3a06c2db419897be8dc6cb8a68970ab
ahmed2016170053 <ahmed2016170053@cis.asu.edu.eg>
2024-08-24 00:30:56 +0300

    -- enhancce design for bookForm

--- Bookify.Web/Core/ViewModels/BookFormViewModel.cs
+++ Bookify.Web/Core/ViewModels/BookFormViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using UoN.ExpressiveAnnotations.NetCore.Attributes;
 
 namespace Bookify.Web.Core.ViewModels
 {
@@ -7,9 +8,12 @@ namespace Bookify.Web.Core.ViewModels
         public int Id { get; set; }
 
         [MaxLength(500, ErrorMessage = Errors.MaxLength)]
+        [Remote("AllowItem", null!, AdditionalFields = "Id,AuthorId", ErrorMessage = Errors.DuplicatedBook)]
         public string Title { get; set; } = null!;
 
+        [Remote("AllowItem", null!, AdditionalFields = "Id,Title", ErrorMessage = Errors.DuplicatedBook)]
         [Display(Name = "Author")]
+
         public int AuthorId { get; set; }
 
         public IEnumerable<SelectListItem>? Authors { get; set; }
@@ -18,6 +22,7 @@ namespace Bookify.Web.Core.ViewModels
         public string Publisher { get; set; } = null!;
 
         [Display(Name = "Publishing Date")]
+        [AssertThat("PublishingDate <= Today()" , ErrorMessage = Errors.NotAllowFutureDates)]
         public DateTime PublishingDate { get; set; } = DateTime.Now;
 
         public IFormFile? Image { get; set; }
