2b012d8510c798b6e8a9b42e159489989f898cdc
ahmed2016170053 <ahmed2016170053@cis.asu.edu.eg>
2024-11-25 00:07:02 +0200

    --add BookCopies Module

--- Bookify.Web/Core/Mapping/MappingProfile.cs
+++ Bookify.Web/Core/Mapping/MappingProfile.cs
@@ -29,6 +29,11 @@ namespace Bookify.Web.Core.Mapping
                  .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Author!.Name))
                  .ForMember(dest => dest.Categories, 
                      opt => opt.MapFrom(src => src.Categories.Select(c => c.Category!.Name).ToList()));
+
+            CreateMap<BookCopy, BookCopyViewModel>()
+                 .ForMember(dest => dest.BookTitle, opt => opt.MapFrom(src => src.Book!.Title));
+
+            CreateMap<BookCopy, BookCopyFormViewModel>();
         }
     }
 }
